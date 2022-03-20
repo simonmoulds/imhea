803cf8ca2f69062386b1ce6049a83e9e584fdd7f
simonmoulds <sim.moulds@gmail.com>
2022-03-20 14:51:40 +0000

    setting up aggregation method for precipitation

--- R/precipitation.R
+++ R/precipitation.R
@@ -2,9 +2,17 @@
 #'
 #' Load iMHEA precipitation object from file.
 #'
-#' @param x Character.
+#' @param file Character. Path to input precipitation data.
+#' @param tz Character. Timezone of recorded times. Defaults to
+#'   "Etc/GMT-5" (i.e. timezone of Peru and Ecuador)
+#' @param lat Numeric.
+#' @param lon Numeric.
 #' @param gauge_type Character.
 #' @param bucket_volume Numeric vector.
+#' @param date_column Character. The name of the date column. Default
+#'   is "Date"
+#' @param event_column Character. The name of the event column.
+#'   Default is "Event mm"
 #' @param ... Additional arguments.
 #'
 #' @return A precipitation object.
@@ -15,30 +23,27 @@
 #' }
 precipitation <- function(file,
                           tz = "Etc/GMT-5",
-                          lat, lon,
+                          lat,
+                          lon,
                           gauge_type,
                           bucket_volume,
                           date_column = "Date",
                           event_column = "Event mm") {
-  ## FOR TESTING:
-  file = "inst/extdata/iMHEA_raw/HUA/iMHEA_HUA_01_PD_01_raw.csv"
+  ## ## FOR TESTING:
+  ## file = "inst/extdata/iMHEA_raw/HUA/iMHEA_HUA_01_PD_01_raw.csv"
   x <- readr::read_csv(file)
   tz <- "Etc/GMT-5"
   try(tz <- lutz::tz_lookup_coords(lat, lon, method = "accurate"), silent = TRUE)
   times <- x[[date_column]] %>% as.POSIXct(tz = tz, format = "%d/%m/%Y %H:%M:%S")
   events <- x[[event_column]]
-  events_cleaned <- remove_repetitive_tips(events, times)
+  events_cleaned <- remove_consecutive_tips(events, times)
 }
 
-## precipitation_qc <- function(x) {
-##   ## Apply quality control procedure to precipitation data
-##   NULL
-## }
-
-remove_repetitive_tips <- function(events, times) {
-  ## Remove repetitive tips above maximum intensity
+remove_consecutive_tips <- function(events, times) {
+  ## Remove consecutive tips above maximum intensity
+  ## Clone of iMHEA_Depure.m
   nd = 86400
-  min_tip_interval = 1.1 / nd
+  min_tip_interval = 1.1 / nd # TODO make this a package option
   ## Time between tips
   intervals = int_length(int_diff(times))
   ## Add arbitrary long interval at the beginning of the time series
@@ -46,46 +51,23 @@ remove_repetitive_tips <- function(events, times) {
   ## Identify tips separated by less than min_tip_interval
   idx = which(intervals <= min_tip_interval) + 1
   events[idx] = 0.
+  ## %% PRINT RESULTS
+  ## fprintf('Removing tips occurring faster than MinT = %6.2f seconds.\n',MinT*86400)
+  ## fprintf('Number of tips identified: %4i.\n',length(find(EventDiff)))
+  ## fprintf('Rainfall volume before depuration: %8.2f mm.\n',nansum(Event_mm))
+  ## fprintf('Rainfall volume after depuration: %8.2f mm.\n',nansum(NewEvent_mm))
+  ## fprintf('\n')
   events
 }
 
-## function [NewEvent_mm] = iMHEA_Depure(Event_Date,Event_mm)
-## %iMHEA Depuration of repetitive tips above maximum intensity.
-## % [NewEvent_Date,NewEvent_mm] =
-## % iMHEA_AggregationDepure(Event_Date,Event_mm).
-## %
-## % Input:
-## % Event_Date    = dd/mm/yyyy hh:mm:ss [date format].
-## % Event_mm      = Precipitation tips [mm].
-## %
-## % Output:
-## % NewEvent_mm   = Precipitation tips, repetitions replaced by zero [mm].
-## %
-## % Boris Ochoa Tocachi
-## % Imperial College London
-## % Created in August, 2017
-## % Last edited in November, 2017
+rain_gauge_network <- function(..., target_resolution) {
+  ## TODO logging
+  dots = list(...)
+  n_gauges = length(dots)
 
+}
 
-## %% INITIALISE VARIABLES
-## fprintf('\n')
-## fprintf('DEPURATION OF REPETITIVE RAINFALL TIPS ABOVE MAXIMUM INTENSITY.\n')
-## nd = 86400; % Number of seconds per day
-## % Minimum tip interval to merge events (slightly greater than 1 second).
-## MinT = 1.1/nd;
-
-## %% IDENTIFY AND REMOVE REPETITIVE EVENTS
-## % Calculate the time between tips.
-## Diff_Event_Date = diff(Event_Date);
-## Diff_Event_Date = [MinT*100;Diff_Event_Date];
-## % Identify tips separated by less than the minimum time MinT.
-## EventDiff = Diff_Event_Date <= MinT;
-## NewEvent_mm = Event_mm;
-## NewEvent_mm(EventDiff) = 0;
-
-## %% PRINT RESULTS
-## fprintf('Removing tips occurring faster than MinT = %6.2f seconds.\n',MinT*86400)
-## fprintf('Number of tips identified: %4i.\n',length(find(EventDiff)))
-## fprintf('Rainfall volume before depuration: %8.2f mm.\n',nansum(Event_mm))
-## fprintf('Rainfall volume after depuration: %8.2f mm.\n',nansum(NewEvent_mm))
-## fprintf('\n')
+aggregation_cs <- function(x) {
+  ## TODO this should be a method for rain_gauge objects
+  NULL
+}
